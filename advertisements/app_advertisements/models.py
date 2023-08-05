4045429df2935155028071fdabd2c974ca2dc211
Lock <arif.latypov07@gmail.com>
2023-08-05 15:23:41 +0300

    Home_work_7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    
+    # строковое поле для небольших размеров
+    # 'заголовок' - verbose_name - название поля извне
+    title = models.CharField('заголовок', max_length=128)
+
+    # Описание товара/информация о товаре
+    # большое текстовое поле, для больших размеров
+    description = models.TextField('описание')
+    
+    
+    # Цена
+    # специальный тип данных с фиксированной точкой
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    
+    # Актуальность обьявления
+    # логический тип, два значения - правда/ложь
+    auction = models.BooleanField('торг', help_text='Отметьте, уместен ли торг')
+    
+    # Дата публикакции
+    # поле записывается при создании объявления.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Дата изменения/обновления
+    # поле записывается при каждом обновлении
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = 'advertisements' 
+    # Имя продавца + контакты
+
+    # Количество товара
+
+    # Уместен ли торг
+
+    # Возможен ли обмен 
+
+    # Адрес продажи/осмотре
+
+    # Б/У или 
