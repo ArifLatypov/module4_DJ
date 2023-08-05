4a1e33db6f818c4c162cef6d1249e6ee6119bd9c
Lock <arif.latypov07@gmail.com>
2023-08-05 16:45:37 +0300

    Home_work_8

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Advertisement(models.Model):
     
@@ -27,6 +28,30 @@ class Advertisement(models.Model):
     # Дата изменения/обновления
     # поле записывается при каждом обновлении
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_date = self.created_at.strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color:green">Сегодня в {} </span>', 
+                created_date
+                )
+        
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата изменения')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_at = self.updated_at.strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color:red">Сегодня в {} </span>', 
+                updated_at
+                )
+        
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
     
     def __str__(self):
         return f"Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})"
